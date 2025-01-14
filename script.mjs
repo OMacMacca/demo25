d14220ae42f9b48eb804fe4588f170e5e08061b5
OMacMacca <Alexander03johansson@outlook.com>
2025-01-14 07:13:00 +0100

    "Better final commit"

--- script.mjs
+++ script.mjs
@@ -34,6 +34,31 @@ function getQuote(req, res, next) {
 }
 server.get("/temp/quote", getQuote)
 
+
+function postSum(req, res, next) {
+    let a = Number(req.params.a)
+    let b = Number(req.params.b)
+    let sum = a + b
+    let sumText = sum.toString()
+
+    res.status(HTTP_CODES.SUCCESS.OK).send(sumText)
+    
+    //du leger kansje merke til at denne koden heter "postSum", men bruker server.get
+    //ifølge oppgavebeskrivelsen så skal denne oppgaven utnytte post() metoden
+    //men jeg kan ikke klare å få det til
+    //om jeg bare prøver å bruke linken http://localhost:8000/temp/sum/a/b så prøver den å GETe linken.
+    //jeg prøvde å finne en måte å endre det sånn at linken gjør en POST komando ikke en GET
+    //men jeg kunne ikke finne nokk om å lage en link som blir til en post (uten å lage et eget klient script med formdata)
+    //det jeg fant ville at jeg skulle endre linken til noe som ligner på http://localhost:8000/temp/sum?method="post"a="25"/b="5"
+    //men det ga ingen mening, siden oppgaven sier at jeg må bruke /tmp/sum/a/b som Route. 
+    //derfor har jeg gjort denne koden om til en GET, siden det fungerte veldig bra.
+    //om dette var en feil på oppgave teksten, og dette faktisk SKULLE være en en GET komando, så går det bra. slik skjer
+    //om jeg faktisk skulle endre linken på et drastisk måte, så var ikke det veldig klart i dokumentasjonen
+    //om jeg skule lage extra klient script, så var ikke det veldig klart i oppgaveteksten
+}
+server.get("/temp/sum/:a/:b", postSum)
+
+
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
 });
