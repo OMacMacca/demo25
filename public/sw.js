d28dd4e6f2d88b755b77ba8b2a485ca5d0caab6a
OMacMacca <Alexander03johansson@outlook.com>
2025-02-23 20:47:49 +0100

    added activate

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,9 @@
-self.addEventListener("install", (e) => {
+//install event
+self.addEventListener("install", e => {
   console.log("Service worker: Installed")
+})
+
+//activate event
+self.addEventListener("activate", e => {
+  console.log("Service worker: Activated")
 })
