c8372f5a70dd1f12b9e5f79777548f26ed1a7faf
OMacMacca <Alexander03johansson@outlook.com>
2025-02-23 20:15:27 +0100

    removed uneeded caches

--- public/sw.js
+++ public/sw.js
@@ -68,12 +68,6 @@ self.addEventListener('install', (event) => {
         './',
         './index.html',
         './style.css',
-        './app.js',
-        './image-list.js',
-        './star-wars-logo.jpg',
-        './gallery/bountyHunters.jpg',
-        './gallery/myLittleVader.jpg',
-        './gallery/snowTroopers.jpg',
       ])
     );
 });
