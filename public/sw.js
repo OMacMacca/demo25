43693e83940850f6d0cd163867b93790923a9119
OMacMacca <Alexander03johansson@outlook.com>
2025-02-23 21:05:03 +0100

    added wait until

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,23 @@
+const cacheName = "v1"
+
+const cacheAssets = [
+  "index.html",
+  "/CSS/styles.css"
+]
+
 //install event
 self.addEventListener('install', e => {
   console.log("Service worker: Installed")
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log("Service worker: Caching Files")
+        cache.addAll(cacheAssets)
+      })
+      .then(() => self.skipWaiting())
+  )
 })
 
 //activate event
