5fb9a1fa41f7164e9a7ef258b1dd84ffb5a4f910
OMacMacca <Alexander03johansson@outlook.com>
2025-03-11 08:52:08 +0100

    fixed wrong referent

--- controlers/deckControler.mjs
+++ controlers/deckControler.mjs
@@ -4,7 +4,7 @@ import baseAuth from '../modules/basicAuthentication.mjs';
 
 const deckRouter = express.Router()
 
-import ItemStore from '../Data/recordStoreAbstractInterface.mjs';
+import ItemStore from '../Data/deckRecordStore.mjs';
 const storageHandler = new ItemStore();
 
 
