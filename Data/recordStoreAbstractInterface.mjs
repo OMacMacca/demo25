24e83d3313523320ce2b10f391e0014788e20552
OMacMacca <Alexander03johansson@outlook.com>
2025-03-11 02:02:16 +0100

    removed error from create

--- Data/recordStoreAbstractInterface.mjs
+++ Data/recordStoreAbstractInterface.mjs
@@ -10,7 +10,7 @@ function RecordStoreAbstractInterface() {
 
 
 
-function create(item) { throw Error("Not implemented") }
+function create(item) {}
 function update(item) { throw Error("Not implemented") }
 function read(id) { throw Error("Not implemented") }
 function readAllIds() { throw Error("Not implemented") }
