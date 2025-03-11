7dcc0af187ebfc5ac8598b0d6d4918f0f091340d
OMacMacca <Alexander03johansson@outlook.com>
2025-03-11 09:16:03 +0100

    unabstracted the abstraction

--- Data/recordStoreAbstractInterface.mjs
+++ Data/recordStoreAbstractInterface.mjs
@@ -1,10 +1,23 @@
+import DbManager from "./db.mjs";
+
 function RecordStoreAbstractInterface() {
     return { 
-        create(item) {},
-        read(item) {throw Error("Not Implemented")},
-        //readAllIds() {throw Error("Not Implemented")},
-        update(item) {throw Error("Not Implemented")},
-        purge(item) {throw Error("Not Implemented")}
+        create(item) {
+            console.log("object put in: " + item)
+            DbManager.create(`INSERT INTO "public"."decks"("id", "deck") VALUES($1, $2) RETURNING "id", "deck";`, item)
+        },
+        read(item) {
+            DbManager.read(`SELECT * FROM "decks" WHERE "id" = $1 RETURNING "id", "deck"`, item.id)
+        },
+        /*readAllIds() {
+            DbManager.read(`SELECT * FROM "decks" WHERE "id" = $1 RETURNING "id", "deck"`, item.id)
+        },*/
+        update(item) {
+            DbManager.update(`UPDATE "public"."decks" SET "deck" = $2 WHERE "id" = $1 RETURNING "id", "deck";`, item)
+        },
+        purge(item) {
+            DbManager.delete(`DELETE FROM decks WHERE "id" = $1 RETURNING "id", "deck"`, item.id)
+        }
     }
 }
 
