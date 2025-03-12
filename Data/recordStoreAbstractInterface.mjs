e1374ea2a8f816ed036dcf364750cefb479e0aea
OMacMacca <Alexander03johansson@outlook.com>
2025-03-12 04:12:02 +0100

    commented out error

--- Data/recordStoreAbstractInterface.mjs
+++ Data/recordStoreAbstractInterface.mjs
@@ -4,10 +4,10 @@ function RecordStoreAbstractInterface() {
 
 
 
-function create(item) {throw Error("Not Implemented")}
-function read(id) {throw Error("Not Implemented")}
-function readAllIds() {throw Error("Not Implemented")}
-function update(item) {throw Error("Not Implemented")}
-function purge(id) {throw Error("Not Implemented")}
+function create(item) {/*throw Error("Not Implemented")*/}
+function read(id) {/*throw Error("Not Implemented")*/}
+function readAllIds() {/*throw Error("Not Implemented")*/}
+function update(item) {/*throw Error("Not Implemented")*/}
+function purge(id) {/*throw Error("Not Implemented")*/}
 
 export default RecordStoreAbstractInterface
