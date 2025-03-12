8dedd501265e17b8dba73f9713c07fa6cfb6bbea
OMacMacca <Alexander03johansson@outlook.com>
2025-03-13 00:58:04 +0100

    removed uneeded return

--- Data/recordStoreAbstractInterface.mjs
+++ Data/recordStoreAbstractInterface.mjs
@@ -7,7 +7,7 @@ function RecordStoreAbstractInterface() {
         create(item) {/*throw Error("Not Implemented")*/},
         read(id) {        
             console.log(id)
-            let fromDB = DbManager.read(`SELECT * FROM "decks" WHERE "id" = $1 RETURNING "id", "deck"`, id)
+            let fromDB = DbManager.read(`SELECT * FROM "decks" WHERE "id" = $1`, id)
             fromDB.deck = CSVtoObject(fromDB.deck)
             console.log(fromDB)
             return fromDB
