6375ff0f02e23a0b6c92c356319a882ff8d82759
OMacMacca <Alexander03johansson@outlook.com>
2025-03-13 00:37:21 +0100

    added direct function to the abstraction

--- Data/recordStoreAbstractInterface.mjs
+++ Data/recordStoreAbstractInterface.mjs
@@ -1,19 +1,21 @@
 function RecordStoreAbstractInterface() {
     return { 
-        create, 
-        update,
-        read, 
-        readAllIds, 
-        purge
+        create(item) {/*throw Error("Not Implemented")*/},
+        read(id) {        
+            console.log(id)
+            let fromDB = DbManager.read(`SELECT * FROM "decks" WHERE "id" = $1 RETURNING "id", "deck"`, id)
+            fromDB.deck = CSVtoObject(fromDB.deck)
+            console.log(fromDB)
+            return fromDB
+        },
+        readAllIds() {/*throw Error("Not Implemented")*/},
+        update(item) {/*throw Error("Not Implemented")*/},
+        purge(id) {/*throw Error("Not Implemented")*/}
     }
 }
 
 
 
-function create(item) {/*throw Error("Not Implemented")*/}
-function read(id) {/*throw Error("Not Implemented")*/}
-function readAllIds() {/*throw Error("Not Implemented")*/}
-function update(item) {/*throw Error("Not Implemented")*/}
-function purge(id) {/*throw Error("Not Implemented")*/}
+
 
 export default RecordStoreAbstractInterface
