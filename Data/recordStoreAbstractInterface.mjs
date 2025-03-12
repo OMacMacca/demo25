764c02fb80e90f83024f7558d5c6828a2bf728d7
OMacMacca <Alexander03johansson@outlook.com>
2025-03-12 04:57:07 +0100

    improved accessability

--- Data/recordStoreAbstractInterface.mjs
+++ Data/recordStoreAbstractInterface.mjs
@@ -1,5 +1,10 @@
 function RecordStoreAbstractInterface() {
-    return { create, update, read, readAllIds, purge}
+    return { 
+        create, 
+        update,
+        read, 
+        readAllIds, 
+        purge}
 }
 
 
