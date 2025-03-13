5ca75a1bf58b185d47ac70b2506aa6626a8fd991
OMacMacca <Alexander03johansson@outlook.com>
2025-03-13 01:08:57 +0100

    added another console.log

--- Data/recordStoreAbstractInterface.mjs
+++ Data/recordStoreAbstractInterface.mjs
@@ -7,6 +7,7 @@ function RecordStoreAbstractInterface() {
         create(item) {/*throw Error("Not Implemented")*/},
         read(id) {        
             let fromDB = DbManager.read(`SELECT * FROM "decks" WHERE "id" = $1`, id)
+            console.log(fromDB)
             fromDB.deck = CSVtoObject(fromDB.deck)
             console.log(fromDB)
             return fromDB
