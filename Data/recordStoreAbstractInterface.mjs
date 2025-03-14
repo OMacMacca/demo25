6f7ae0c91179ff197c697e7ed2f66e1435edc630
OMacMacca <Alexander03johansson@outlook.com>
2025-03-14 01:11:19 +0100

    made abstraction a class

--- Data/recordStoreAbstractInterface.mjs
+++ Data/recordStoreAbstractInterface.mjs
@@ -3,54 +3,21 @@ import { objectToCSV } from "./csvHandlers/ObjToCsv.mjs";
 import { CSVtoObject } from "./csvHandlers/CsvToObj.mjs";
 
 
-//i had a lot of problems extending this function's methods into a new class, so i baked them into the function directly. 
-//i know its not best practice for abstractions, but i wanted to get it done
-function RecordStoreAbstractInterface() {
-    return { 
-        create(item) {
-            item.deck = objectToCSV(item.deck)
-            let fromDB = DbManager.create(`INSERT INTO "public"."decks"("id", "deck") VALUES($1, $2) RETURNING "id", "deck";`, item.id, item.deck)
-            .then((promise) => {
-                promise.deck = promise.deck.replace(/\r/g, '')
-                promise.deck = CSVtoObject(promise.deck)
-                return promise
-            })
-            return fromDB
-        },
-        read(id) {
-            let fromDB =  DbManager.read(`SELECT * FROM "decks" WHERE "id" = $1`, id)
-            .then((promise) => {
-                promise.deck = promise.deck.replace(/\r/g, '')
-                promise.deck = CSVtoObject(promise.deck)
-                return promise
-            })
-            return fromDB 
-        },
-        readAllIds() {
-            let fromDB = DbManager.readAllIds(`SELECT "id" FROM "decks"`)
-            .then((promise) => {
-                return promise
-            })
-            return fromDB
-        },
-        update(item) {            
-            item.deck = objectToCSV(item.deck)
-            let fromDB = DbManager.create(`UPDATE "public"."decks" SET "deck" = $2 WHERE "id" = $1 RETURNING "id", "deck";`, item.id, item.deck)
-            .then((promise) => {
-                promise.deck = promise.deck.replace(/\r/g, '')
-                promise.deck = CSVtoObject(promise.deck)
-                return promise
-            })
-            return fromDB
-        },
-        purge(id) {
-            let fromDB = DbManager.purge(`DELETE FROM decks WHERE "id" = $1 RETURNING "id", "deck"`, id)
-            .then((promise) => {
-                promise.deck = CSVtoObject(promise.deck)
-                return promise
-            })
-            return fromDB
-        }
+class RecordStoreAbstractInterface {
+    create(item) {
+        throw new Error("Method 'create' must be implemented.");
+    }
+    read(id) {
+        throw new Error("Method 'read' must be implemented.");
+    }
+    update(item) {
+        throw new Error("Method 'update' must be implemented.");
+    }
+    purge(id) {
+        throw new Error("Method 'purge' must be implemented.");
+    }
+    readAllIds() {
+        throw new Error("Method 'readAllIds' must be implemented.");
     }
 }
 
